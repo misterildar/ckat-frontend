c4cd3c15bf5a00c08fb8abb99673a4793074cdd0
Ildar <misterildar@gmail.com>
2024-06-09 08:14:56 +0300

    fix fome height

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,40 +11,36 @@ export const Home: FC = () => {
 
   const isMobile = device === 'mobile';
 
-  const styleImage = isMobile ? styles.home__image_mobile : styles.home__image;
-
-  const styleHome = isMobile ? styles.home__mobile : styles.home;
-
   const styleGrid = isMobile ? styles.home__grid_mobile : styles.home__grid;
 
+  const styleImage = isMobile ? styles.home__image_mobile : styles.home__image;
+
   const fontSize = isMobile ? (
-    <h4 className={styles.home__title_bottom}>
+    <h4 className={styles.home__title}>
       Работаем с&nbsp;2012&nbsp;года, в&nbsp;наличии необходимые товары для
       электромонтажа и&nbsp;безопасности, а&nbsp;также многое другое.
     </h4>
   ) : (
-    <h2 className={styles.home__title_bottom}>
-      Работаем с&nbsp;2012&nbsp;года, в&nbsp;наличии необходимые товары для
-      электромонтажа и&nbsp;безопасности, а&nbsp;также многое другое.
+    <h2 className={styles.home__title}>
+      Работаем с&nbsp;2012&nbsp;года. Проконсультируем вас если возникнут
+      вопросы. Подберем именно те&nbsp;товары, которые обеспечат максимально
+      удобный монтаж, надежную защиту вас, ваших близких и&nbsp;вашего дома.
+      Здесь вы&nbsp;можете подобрать розетки и&nbsp;выключатели различных цветов
+      и&nbsp;дизайна. Если нужных цветов или количества вдруг не&nbsp;окажется
+      в&nbsp;наличии оперативно привезем их&nbsp;для вас в&nbsp;течении
+      2&nbsp;&mdash; 3&nbsp;рабочих дня
     </h2>
   );
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styleHome}>
-        <Slider />
-        <div className={styleGrid}>
-          {arraySvg.map((svg, index) => (
-            <img
-              className={styleImage}
-              src={svg}
-              alt='photoImage'
-              key={index}
-            />
-          ))}
-        </div>
-        <div className={styles.home__bottom}>{fontSize}</div>
+    <div className={styles.home}>
+      <Slider />
+      <div className={styleGrid}>
+        {arraySvg.map((svg, index) => (
+          <img className={styleImage} src={svg} alt='photoImage' key={index} />
+        ))}
       </div>
+      <div className={styles.home__bottom}>{fontSize}</div>
     </div>
   );
 };
